dfb2cd190b6209e158dd03d08601da6b87ea45ed
Felix Liberman <felix.liberman@gmail.com>
2022-04-24 22:03:13 +0300

    progress bar for new page load

--- htdocs/personal/read.php
+++ htdocs/personal/read.php
@@ -518,6 +518,22 @@ $rss_app->showItems($items);
       </div>
     </div>
 
+    <div class="modal" id="processingDialog" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">processing, please wait...</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <img style="display: block; margin-left: auto; margin-right: auto;" src="../img/processing_bar.gif" >
+          </div>
+          <div class="modal-footer">
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="modal fade" id="updatedDialog" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
