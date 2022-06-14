10a6555c33124c02476f7351c79825f83da1412e
Felix Liberman <felix.liberman@gmail.com>
2022-06-14 20:12:49 +0300

    Correct title for new subscr. modal window

--- htdocs/personal/add_new_rss.php
+++ htdocs/personal/add_new_rss.php
@@ -106,7 +106,7 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Add new script</h5>
+            <h5 class="modal-title">Add new RSS channel</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body" id="modal-message">
